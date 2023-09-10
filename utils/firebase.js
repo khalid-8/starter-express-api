0f7ab156f53b27d561e011d1fb75206554b5516e
khalid-8 <kalnahdi1@gmail.com>
2023-09-10 09:47:25 +0300

    update

--- utils/firebase.js
+++ utils/firebase.js
@@ -4,10 +4,23 @@ const {getAuth} = require("firebase-admin/auth")
 
 require('dotenv').config()
 
-const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const cred = {
+  "type": process.env.DB_TYPE,
+  "project_id": process.env.DB_PROJECT_ID,
+  "private_key_id": process.env.DB_PRIVATE_KEY_ID,
+  "private_key": process.env.DB_PRIVATE_KEY,
+  "client_email": process.env.DB_CLIENT_EMAIL,
+  "client_id": process.env.DB_CLIENT_ID,
+  "auth_uri": process.env.DB_AUTH_URI,
+  "token_uri": process.env.DB_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.DB_AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.DB_CLIENT_X509_CERT_URL,
+  "universe_domain" : process.env.DB_UNIVERSE_DOMAIN
+}
+
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(cred),
 });
 
 
